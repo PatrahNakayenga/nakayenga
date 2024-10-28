feff48d5443261dc8bee31088e7812d565a414a1
PatrahNakayenga <85937187+PatrahNakayenga@users.noreply.github.com>
2024-10-28 19:11:17 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,9 @@
 
+$(document).ready(function() {
+  // When the .nav-icon is clicked, toggle the 'open' class on the nav-links
+  $(".nav-icon").on("click", function() {
+    $(".nav-links").toggleClass("open");
+  });
+});
+
+
