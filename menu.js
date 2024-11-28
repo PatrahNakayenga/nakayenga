37785234f0346e0ae5a13ceaf7eff721df641baf
PatrahNakayenga <85937187+PatrahNakayenga@users.noreply.github.com>
2024-11-28 09:53:20 +0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,4 +1,4 @@
-
+/*
 $(document).ready(function() {
   // When the .nav-icon is clicked, toggle the 'open' class on the nav-links
   $(".nav-icon").on("click", function() {
@@ -7,3 +7,13 @@ $(document).ready(function() {
 });
 
 
+*/
+
+
+$(document).ready(function() {
+  // When the .nav-icon is clicked, toggle the 'open' class on the nav-icon and nav-links
+  $(".nav-icon").on("click", function() {
+    $(".nav-icon").toggleClass("open");  // Toggle 'open' class on the nav-icon to change the icon
+    $(".nav-links").toggleClass("open"); // Toggle 'open' class on the nav-links to show/hide the menu
+  });
+});
